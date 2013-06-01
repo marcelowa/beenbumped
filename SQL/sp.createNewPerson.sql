ac21a8584eda0725015d9bf5185af56bb32134fd
Marcelo Waisman <marcelo.waisman@gmail.com>
2013-06-01 17:05:22 +0300

    - added file helps to create the stored procedure sp_createNewPerson

---
+++ SQL/sp.createNewPerson.sql
@@ -0,0 +1,68 @@
+-- =============================================
+-- Description: Create a new person
+-- Note: all arguments must be passed to the proc since mysql doesn't support optional arguments
+-- =============================================
+DELIMITER $$
+USE beenbumped$$
+DROP PROCEDURE IF EXISTS sp_createNewPerson$$
+CREATE PROCEDURE sp_createNewPerson (
+	IN email VARCHAR(320),
+	IN firstName VARCHAR(30),
+	IN lastName VARCHAR(30),
+	IN city VARCHAR(30),
+	IN streetName VARCHAR(50),
+	IN houseNumber SMALLINT,
+	IN addressDetails VARCHAR(64),
+	IN zipcode INT,
+	IN phone1 VARCHAR(20),
+	IN phone2 VARCHAR(20) ,
+	IN insuranceCompany VARCHAR(64),
+	IN insuranceAgentName VARCHAR(64),
+	IN insurancePhone1 VARCHAR(20),
+	IN insurancePhone2 VARCHAR(20),
+	IN insuranceNumber VARCHAR(20),
+	IN created DATETIME,
+	IN modified DATETIME
+)
+BEGIN
+INSERT INTO beenbumped.persons (
+	email,
+	firstName,
+	lastName,
+	city,
+	streetName,
+	houseNumber,
+	addressDetails,
+	zipcode,
+	phone1,
+	phone2,
+	insuranceCompany,
+	insuranceAgentName,
+	insurancePhone1,
+	insurancePhone2,
+	insuranceNumber,
+	created,
+	modified
+)
+VALUES (
+	email,
+	firstName,
+	lastName,
+	city,
+	streetName,
+	houseNumber,
+	addressDetails,
+	zipcode,
+	phone1,
+	phone2,
+	insuranceCompany,
+	insuranceAgentName,
+	insurancePhone1,
+	insurancePhone2,
+	insuranceNumber,
+	created,
+	modified
+);
+END$$
+
+DELIMITER ;
