0cf289f11f604f5f9bf54f791f8a1c327187d03e
Marcelo Waisman <marcelo.waisman@gmail.com>
2013-07-02 00:40:39 +0300

    - continue with register

--- WebContent/js/controllers.js
+++ WebContent/js/controllers.js
@@ -21,8 +21,39 @@ function UserLoginCtrl($scope, $location, User) {
 	};
 }
 
-function UserRegisterCtrl($scope) {
-
+function UserRegisterCtrl($scope, $location, User) {
+	$scope.user = {
+		username : "marc@walla.com",
+		email : "marc@walla.com",
+		password : "stamstam",
+		passwordConfirm : "stamstam"
+	};
+	
+	$scope.register = function(user) {
+		$scope.user = User.save(user, function(response){
+			console.log(response);
+			console.log($scope.user);
+			//TODO store userid and hash in cookies or in service for next controller
+		}, function(response){
+			console.error(response);
+			//TODO show failure in login form
+		});
+	};
+	
+	$scope.isInvalid = function(user) {
+		var invalid = typeof user.password == "undefined"
+			|| user.password != user.passwordConfirm 
+			|| user.username  == "undefined"
+			|| user.username != user.email;
+		return invalid;
+	};
+	
+	/*
+	$scope.isUnchanged(user) {
+		return angular.equals(user, $scope.user);
+	};
+	*/
+	
 }
 
 function UserEditCtrl($scope, $routeParams, $cookies) {
--- WebContent/js/services.js
+++ WebContent/js/services.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular
-	.module('beenbumpedServices', ['ngResource'])
-	.factory('User', function($resource){
-		return $resource('/beenbumped/rest/user');
+	.module("beenbumpedServices", ["ngResource"]).config(function ($httpProvider) {
+	    $httpProvider.defaults.transformRequest = function (data) {
+	        var str = [];
+	        for (var p in data) {
+	            data[p] !== undefined && str.push(encodeURIComponent(p) + "=" + encodeURIComponent(data[p]));
+	        }
+	        return str.join('&');
+	    };
+	    $httpProvider.defaults.headers.put["Content-Type"] = $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8";
+	}).factory("User", function($resource, $http){
+		return $resource("/beenbumped/rest/user");
 	});
